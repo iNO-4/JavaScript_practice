78a6ddd7c83ec943a8a90209df8187a7fd6ac4ba
猪原将志 <masa4910@inoharamasashi-no-MacBook.local>
2020-11-09 19:45:35 +0900

    practice_3

---
+++ JS_practice_3/app.js
@@ -0,0 +1,15 @@
+// JSの基本的な文法(変数、四則演算、条件分岐、繰り返し処理)
+// JSもプログラミング言語なので、基本的な考えはRubyと一緒。
+
+// Rubyの場合、変数をかく場合は、
+// hello = "Hello, World"
+// puts hello
+// 実行結果は..."Hello, World"となる
+// JSでは、変数名の前に「var」をつけることで、変数を宣言できる。
+// var hello = "Hello, World";
+
+// var 変数名 = 内容 ;
+// varはvariableの略で、varの後ろに変数名と内容(初期値)を書く。
+
+var hello = 'Hello World';
+alert(hello);
